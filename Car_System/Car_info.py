0cdd20c585f2e25d2b9a0c97e55524d2da8e6765
kazu314159265 <kazumaito959616@gmail.com>
2023-03-13 10:53:18 +0900

    a

--- Car_System/Car_info.py
+++ Car_System/Car_info.py
@@ -11,63 +11,6 @@ https://qiita.com/taka-kawa/items/673716d77795c937d422
 """
 
 
-class MCP3208:
-    """
-    ADコンバータICのMCP3208を制御するクラス.MCP3208はMicrochip社の12bitのADコンバーターSPIインターフェースで制御できる。
-    ADコンバーターのシーケンスは下記を参照.
-    https://akizukidenshi.com/download/MCP3208.pdf
-    """
-
-    channel = 1
-    baud = 50000
-    flags = 0
-
-    def __init__(self, pi, ref_volt=3.3):
-        """
-        初期化処理. spiデバイスのオープンをしたりする.
-
-        Parameters
-        ----------
-        pi : class
-            外部でインスタンス化したpigpio.piのクラスオブジェクトを代入する変数
-        ref_volt : float
-            基準電圧の値
-        """
-        self.pi = pi
-        self.ref_volt = ref_volt  # 基準電圧設定
-        self.hndl = pi.spi_open(self.channel, self.baud, self.flags)  # デバイスオープン
-
-    def AnalogIn(self, Channel=0):
-        """
-        ADコンバータから測定値を取得するメソッド.
-
-        Parameters
-        ----------
-        Channel : int
-            アナログ入力をするチャンネルを指定 0-7の範囲
-
-        Returns
-        -------
-        voltage : float
-            コンストラクタで設定した基準電圧に換算した電圧値
-        """
-        if not 0 <= Channel < 8:
-            raise SPIBadChannel(
-                "channel must be between 0 and 7"
-            )  # チャンネル数が0-7の範囲にあるかチェック
-
-        cmnd = [(0b00000110 + int(Channel / 4)), ((Channel & 0b00000011) << 6), 0]
-        c, row = self.pi.spi_xfer(self.hndl, cmnd)
-        voltage = (((row[1] & 0b00001111) << 8) + row[2]) * self.ref_volt / 4096
-        return voltage
-
-    def Cleanup(self):
-        """
-        spiデバイスをクローズする関数
-        """
-        self.pi.spi_close(self.hndl)
-
-
 class Car_info:
     """
     車の情報を取得, 管理するクラス
@@ -184,7 +127,3 @@ class Car_info:
 
     def poring():
         pass
-
-
-class SPIBadChannel(Exception):
-    pass
